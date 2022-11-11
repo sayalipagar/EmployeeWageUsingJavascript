2abcc29582392ba80788e3997a6c1b019689a464
sayalipagar <pagarsayali565@gmail.com>
2022-11-11 12:19:52 +0530

    [Sayali]:StoredDailyWage

--- EmpolyeeWage.js
+++ EmpolyeeWage.js
@@ -1,4 +1,4 @@
-//UC5-Calculate Wage till No of working days or total working hours per Month is reached
+//UC6-Stored Daily Wages
 const NUM_OF_WORKING_DAYS = 20;
 const MAX_HRS_IN_MONTH = 160;
 const IS_PART_TIME = 1;
@@ -7,8 +7,10 @@ const PART_TIME_HOURS = 4;
 const FULL_TIME_HOUR = 8;
 const WAGE_PER_HOUR= 20;
 
+let empHrs = 0
 let totalEmpHrs = 0;
 let totalWorkingDays = 0;
+let empDailyWageArr = new Array();
 function getworkingHours(empCheck) {
     switch(empCheck) {
         case IS_PART_TIME:
@@ -19,11 +21,16 @@ function getworkingHours(empCheck) {
             return 0;
     }
 }
+function calculateDailyWage(empHrs) {
+    return empHrs * WAGE_PER_HOUR;
+}
 while (totalEmpHrs <= MAX_HRS_IN_MONTH && 
         totalWorkingDays < NUM_OF_WORKING_DAYS) {
             totalWorkingDays++;
             let empCheck = Math.floor(Math.random() * 10) % 3;
             totalEmpHrs += getworkingHours(empCheck);
+            totalEmpHrs += empHrs;
+            empDailyWageArr.push(calculateDailyWage(empHrs));
 }
-let empWage = totalEmpHrs * WAGE_PER_HOUR;
+let empWage = calculateDailyWage(totalEmpHrs);
 console.log("Total Days : " +totalWorkingDays+ "\tTotal Hours: "+totalEmpHrs+ "\tEmployee Wage: "+empWage);
