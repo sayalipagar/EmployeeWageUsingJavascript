c32db31c70670bfd23b49dc01413fac4fbbd9d07
sayalipagar <pagarsayali565@gmail.com>
2022-11-11 11:56:26 +0530

    [Sayali]:Calculate EmployeeWage

--- EmpolyeeWage.js
+++ EmpolyeeWage.js
@@ -1,11 +1,22 @@
-{
-    console.log("Welcome")
-    const IS_ABSENT=0;
-    let empCheck=Math.floor(Math.random()*10)%2;
-    if(empCheck ==IS_ABSENT){
-        console.log("UC1 : Employee is Absnet")
-        return;
-    }else{
-        console.log("UC2 : Employee is Present");
-    }
+//UC2-Calculate Employee Wage
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HOURS = 4;
+const FULL_TIME_HOUR = 8;
+const WAGE_PER_HOUR= 20;
+
+let empHrs = 0;
+empCheck = Math.floor(Math.random() * 10) % 3;
+switch(empCheck) {
+    case IS_PART_TIME:
+        empHrs = PART_TIME_HOURS;
+        break;
+    case IS_FULL_TIME:
+        empHrs = FULL_TIME_HOUR;
+        break;
+    default:
+        empHrs = 0;
 }
+
+let empWage = empHrs * WAGE_PER_HOUR;
+console.log("Employee Wage is : " +empWage);
