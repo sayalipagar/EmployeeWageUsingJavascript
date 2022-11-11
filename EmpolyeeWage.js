4f5425bfdf6049993c2dfa8fe77297457cfb5ff6
sayalipagar <pagarsayali565@gmail.com>
2022-11-11 11:52:39 +0530

    [Sayali]:Employee is present or absent

---
+++ EmpolyeeWage.js
@@ -0,0 +1,11 @@
+{
+    console.log("Welcome")
+    const IS_ABSENT=0;
+    let empCheck=Math.floor(Math.random()*10)%2;
+    if(empCheck ==IS_ABSENT){
+        console.log("UC1 : Employee is Absnet")
+        return;
+    }else{
+        console.log("UC2 : Employee is Present");
+    }
+}
