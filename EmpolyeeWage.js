641fea4b5610d5c89d1712ee4b52fe0e7e7e7253
sayalipagar <pagarsayali565@gmail.com>
2022-11-11 12:00:29 +0530

    [Sayali]:Refactor Function

--- EmpolyeeWage.js
+++ EmpolyeeWage.js
@@ -1,4 +1,4 @@
-//UC2-Calculate Employee Wage
+//UC3-Refactore the code Function and Calculate Employee Wage
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
@@ -6,17 +6,17 @@ const FULL_TIME_HOUR = 8;
 const WAGE_PER_HOUR= 20;
 
 let empHrs = 0;
-empCheck = Math.floor(Math.random() * 10) % 3;
-switch(empCheck) {
-    case IS_PART_TIME:
-        empHrs = PART_TIME_HOURS;
-        break;
-    case IS_FULL_TIME:
-        empHrs = FULL_TIME_HOUR;
-        break;
-    default:
-        empHrs = 0;
+function getworkingHours(empCheck) {
+    switch(empCheck) {
+        case IS_PART_TIME:
+            return PART_TIME_HOURS;
+        case IS_FULL_TIME:
+            return FULL_TIME_HOUR;
+        default:
+            return 0;
+    }
 }
-
+let empCheck = Math.floor(Math.random() *10) % 3;
+empHrs = getworkingHours(empCheck);
 let empWage = empHrs * WAGE_PER_HOUR;
 console.log("Employee Wage is : " +empWage);
