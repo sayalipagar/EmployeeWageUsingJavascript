2c89e46534ba8f79e23041e1faccf8400585ed8b
sayalipagar <pagarsayali565@gmail.com>
2022-11-11 12:16:11 +0530

    [Sayali]:CalculateWages

--- EmpolyeeWage.js
+++ EmpolyeeWage.js
@@ -1,12 +1,14 @@
-//UC4-Calculate Wage For Month
+//UC5-Calculate Wage till No of working days or total working hours per Month is reached
 const NUM_OF_WORKING_DAYS = 20;
+const MAX_HRS_IN_MONTH = 160;
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOUR = 8;
 const WAGE_PER_HOUR= 20;
 
-let empHrs = 0;
+let totalEmpHrs = 0;
+let totalWorkingDays = 0;
 function getworkingHours(empCheck) {
     switch(empCheck) {
         case IS_PART_TIME:
@@ -17,9 +19,11 @@ function getworkingHours(empCheck) {
             return 0;
     }
 }
-for (let day = 0; day < NUM_OF_WORKING_DAYS; day++) {
-    let empCheck = Math.floor(Math.random() * 10) % 3;
-    empHrs += getworkingHours(empCheck);
+while (totalEmpHrs <= MAX_HRS_IN_MONTH && 
+        totalWorkingDays < NUM_OF_WORKING_DAYS) {
+            totalWorkingDays++;
+            let empCheck = Math.floor(Math.random() * 10) % 3;
+            totalEmpHrs += getworkingHours(empCheck);
 }
-let empWage = empHrs * WAGE_PER_HOUR;
-console.log("Total Hours : " +empHrs+ "\tEmployee Wage: "+empWage);
+let empWage = totalEmpHrs * WAGE_PER_HOUR;
+console.log("Total Days : " +totalWorkingDays+ "\tTotal Hours: "+totalEmpHrs+ "\tEmployee Wage: "+empWage);
